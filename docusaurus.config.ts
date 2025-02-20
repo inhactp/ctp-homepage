128ca14e06ab0144be9a1e5ce25bfcc53d1b8c04
Firo_SF <cmgjol010@gmail.com>
2025-02-20 19:05:20 +0900

    fix: 설정 수정

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,15 +10,18 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  // url: "https://your-docusaurus-site.example.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
+  // baseUrl: "/",
+  url: "https://inhactp.github.io",
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  projectName: "inhactp.github.io",
+  organizationName: "inhactp",
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
